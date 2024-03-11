b93b8f29cc93b7179908e7b41b6304722dd0eb1b
aginamena <aginamena5@gmail.com>
2024-03-11 14:21:08 -0400

    added console logs

--- app/all/page.jsx
+++ app/all/page.jsx
@@ -14,10 +14,11 @@ export const revalidate = 300;
 
 export default async function NikeSneakers(props) {
   let q = null;
-
+  console.log(props);
   if (Object.keys(props.searchParams).length > 0) {
     const { size, color } = props.searchParams;
     if (size && color) {
+      console.log(1);
       q = query(
         collection(firestore, "products"),
         where("productStatus", "==", "Published"),
@@ -27,6 +28,7 @@ export default async function NikeSneakers(props) {
         limit(13)
       );
     } else if (size) {
+      console.log(2);
       q = query(
         collection(firestore, "products"),
         where("productStatus", "==", "Published"),
@@ -35,6 +37,7 @@ export default async function NikeSneakers(props) {
         limit(13)
       );
     } else {
+      console.log(3);
       q = query(
         collection(firestore, "products"),
         where("productStatus", "==", "Published"),
@@ -44,6 +47,7 @@ export default async function NikeSneakers(props) {
       );
     }
   } else {
+    console.log(4);
     q = query(
       collection(firestore, "products"),
       where("productStatus", "==", "Published"),

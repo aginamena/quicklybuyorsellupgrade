953b06a85d352ff46a967a3ca54504f5d52f6a1c
aginamena <aginamena5@gmail.com>
2024-03-17 23:25:33 -0400

    removed undefiend variable

--- app/all/page.jsx
+++ app/all/page.jsx
@@ -1,9 +1,8 @@
 "use client";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Button, Container } from "@mui/material";
 import React, { useState } from "react";
 import Filter from "./Filter";
 import Paginate from "./Paginate";
-import Link from "next/link";
 
 export default function Body() {
   const [drawerCmp, setDrawerCmp] = useState(false);
@@ -25,22 +24,7 @@ export default function Body() {
           filter
         </Button>
       </Container>
-      {top12Products.length == 0 ? (
-        <Box style={{ textAlign: "center" }}>
-          <Typography variant="h6">No products available.</Typography>
-          <Link href="../all" style={{ textDecoration: "none" }}>
-            <Button
-              variant="contained"
-              size="large"
-              style={{ marginTop: "30px" }}
-            >
-              Go back
-            </Button>
-          </Link>
-        </Box>
-      ) : (
-        <Paginate filterCondition={filterCondition} />
-      )}
+      <Paginate filterCondition={filterCondition} />
     </React.Fragment>
   );
 }
